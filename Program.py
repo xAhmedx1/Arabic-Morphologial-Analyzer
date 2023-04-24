a95d77fa5de1f3411d42cd79b4207a8d70cec574
Ahmed Khaled <ahmedkhaled11001@gmail.com>
2023-04-24 14:44:11 +0200

    fixing some issues

--- Program.py
+++ Program.py
@@ -1,122 +1,14 @@
-# Import needed packages
-import os, sys
+# Import modules
+import preprocessing as pre
 
-# -----------------------------------------------------------
-import arabic_reshaper
-from bidi.algorithm import get_display
-# prefix.append(get_display(arabic_reshaper.reshape(t[:-1])))
-import nltk
-from nltk.stem.isri import ISRIStemmer
-from nltk import word_tokenize
-st = ISRIStemmer()
-# nltk.download('punkt')
-# -----------------------------------------------------------
-
-# Main folder path
-dir_path = os.path.join(sys.path[0])
-
-# List to store files names
-extra_c_dir = os.path.join(dir_path, 'files') # the directory of the files that contains the extra characters
-extra_c = []
-
-# Iterate directory
-for path in os.listdir(extra_c_dir):
-    # check if current path is a file
-    if os.path.isfile(os.path.join(extra_c_dir, path)): 
-        extra_c.append(path)
-
-# Creating unique lists, each contains the extra characters of their type
-for i in extra_c:
-    with open(os.path.join(extra_c_dir, i), "r", encoding = "utf-8") as f:
-        if i == 'prefix.txt':
-            d = f.readlines()
-            prefix = []
-            for t in d:
-                prefix.append(t[:-1])
-
-        elif i == 'suffix.txt':
-            d = f.readlines()
-            suffix = []
-            for t in d:
-                suffix.append(t[:-1])
-        elif i == 'special_words.txt':
-            d = f.readlines()
-            special_words = []
-            for t in d:
-                special_words.append(t[:-1])
-        elif i == 'infix.txt':
-            d = f.readlines()
-            infix = []
-            for t in d:
-                infix.append(t[:-1])
-            
-
-lexicon_dir = os.path.join(extra_c_dir, 'lexicon')
-prefix_dir = os.path.join(lexicon_dir, 'prefix')
-suffix_dir = os.path.join(lexicon_dir, 'suffix')
-infix_dir = os.path.join(lexicon_dir, 'infix')
-
-input_phrase = 'ألقي د. أحمد كلمة علي الحضور في أحد القاعات الدراسية و سلم علي صديقه عثمان الكاتب و قال له انهم سيكتبون المقال الأسبوع القادم'
-words_list = input_phrase.split()
+input = "وأيمن"
+i_split = input.split()
 
 root_list = []
 
-for word in words_list:
-    if word in special_words:
-        root_list.append(word)
-    else:
-        w_index = words_list.index(word)
-        special = ''
-        flag = True
-        # the flag job is to iterate in a single word repeatedly until it removes all the extra words in it
-        # ex: 'فسيكتب' = has prefix characters 'ف' and 'س', so it will iterate untill it removes them all
-        while flag:
-            flag = False
-            y, p = '', ''
-            for i in word:
-                y += i
-                if y in prefix:
-                    p = y
-            
-            if p != '':
-                filename = p + '.txt'
-                with open(os.path.join(prefix_dir, filename), "r", encoding = "utf-8") as f:
+for w in i_split:
+    norm_word = pre.normalize(w)
+    wordv2 = pre.pref_handler(norm_word)
+    root_list.append(wordv2)
 
-                        x = f.readlines()
-                        if word + '\n' in x:
-                            # root_list.append(word)
-                            special = word
-                        else:
-                            # root_list.append(st.stem(word))
-                            word = word[len(p):]
-                            flag = True
-
-                            
-        flag = True
-        while flag and special == '':
-            flag = False    
-            y, s = '', ''
-            for i in word[::-1]:
-                y = i + y
-                if y in suffix:
-                    s = y
-            
-            if s != '':
-                filename = s + '.txt'
-                with open(os.path.join(suffix_dir, filename), "r", encoding = "utf-8") as f:
-
-                        x = f.readlines()
-                        if word + '\n' in x:
-                            # root_list.append(word)
-                            special = word
-                        else:
-                            # root_list.append(st.stem(word))
-                            word = word[:-1 * len(s)]
-                            flag = True
-                
-
-        if special != '': root_list.append(special)
-        else: root_list.append(word)
-        
-        
 print(root_list)
---
+++ preprocessing.py
@@ -0,0 +1,240 @@
+# Import Needed Modules
+import os, sys
+
+# Defining folders pathes
+dir_path = os.path.join(sys.path[0])
+files_path = os.path.join(dir_path, 'files')
+lexicon_path = os.path.join(files_path, 'lexicon')
+prefix_path = os.path.join(lexicon_path, 'prefix')
+suffix_path = os.path.join(lexicon_path, 'suffix')
+
+def normalize(word):
+    """
+    Removing diacritics in g given word (except the shaddah '  ّ' the letter that contains it will be duplicated)
+
+    trasfoming the character 'إ'and 'آ' to 'ا'
+    
+    e.g. "كُتَّاب" --> "كتتاب"
+    """
+    with open(os.path.join(files_path, 'characters.txt'), "r", encoding = "utf-8") as f:
+        v = f.readlines()
+    n_word = ""
+    for i in range(len(word)):
+        k = word[i]
+        j = word[i] + '\n'
+        if word[0] in ['إ', 'آ'] and n_word == "":
+            n_word += 'ا'
+        elif k == 'ّ': n_word += word[i-1]
+        elif j in v: n_word += k
+        elif k == 'ى': n_word += 'ي'
+        else: continue
+    return n_word
+
+def ch_func(path, ch):
+    """
+    creating a list of each in a text file of a specified name
+    - path - the path of the folder that contians the file
+    - ch - the character name that we want to create a list of (each file are named as the character's name)
+    """
+    with open(os.path.join(path, ch + '.txt'), "r", encoding = "utf-8") as f:
+                d = f.readlines()
+                ch_list = []
+                for t in d:
+                    ch_list.append(t[:-1])
+    return ch_list
+
+def known_root(x, lexicon_list):
+    for l in lexicon_list:
+        if l in x:
+            return l
+    return False
+
+def check_lexicon(word):
+    r = known_root(word, specialwords_list)
+    if r and len(r) > (len(word) - len(r)):
+        return r
+    elif word[0:2] in prefix_ch:
+        ch_list = ch_func(prefix_path, word[0:2])
+        r = known_root(word, ch_list)
+        if r and len(r) > (len(word) - len(r)):
+            return r
+    elif word[0] in prefix_ch:
+        ch_list = ch_func(prefix_path, word[0])
+        r = known_root(word, ch_list)
+        print(r)
+        if r and len(r) > (len(word) - len(r)):
+            return known_root(word, ch_list)
+        
+    # elif word[-2:] in suffix_ch:
+    #     ch_list = ch_func(suffix_path, word[-2:])
+    #     if known_root(word, ch_list):
+    #         return known_root(word, ch_list)
+    # elif word[-1] in prefix_ch:
+    #     ch_list = ch_func(suffix_path, word[-1])
+    #     if known_root(word, ch_list):
+    #         return known_root(word, ch_list)
+    return False
+
+specialwords_list = ch_func(files_path, 'special_words')
+
+prefix_ch = ['س', 'ال', 'أ', 'ل', 'ب', 'ك', 'ف', 'و']
+suffix_ch = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+
+# Creating a list of each prefix and suffix
+sen_list = ch_func(prefix_path, 'س')
+al_list = ch_func(prefix_path, 'ال')
+hamza_list = ch_func(prefix_path, 'أ')
+lam_list = ch_func(prefix_path, 'ل')
+baa_list = ch_func(prefix_path, 'ب')
+kaph_list = ch_func(prefix_path, 'ك')
+faa_list = ch_func(prefix_path, 'ف')
+waw_list = ch_func(prefix_path, 'و')
+
+# Creating a function for each prefix to check if it is really a prefix based or our roles we put, or not
+def sen_pref(word):
+    """
+    check if the sen 'س' is a prefix of not, and remove it if it was
+    """
+    if len(word) > 3:
+        if check_lexicon(word): return check_lexicon(word)
+        elif word[1] in ['أ', 'ي', 'ت', 'ن']:
+            return word[2:]
+        else: return word
+    else: return word
+
+def al_pref(word):
+    """
+    check if the al 'ال' is a prefix of not, and remove it if it was
+    """
+    if len(word) > 4:
+        if check_lexicon(word): return check_lexicon(word)
+        elif word[2] in ['أ', 'ا', 'آ', 'إ']:
+            return word[2:]
+        else: return word[2:]
+    else: return word
+
+def hamza_pref(word):
+    """
+    check if the hamza 'أ' is a prefix of not, and remove it if it was
+    """
+    if len(word) > 3:
+       if check_lexicon(word): 
+            return check_lexicon(word)
+       elif word[1] == 'أ':
+            return word[1:]
+       else: return 'ا' + word[1:]
+    else: return word
+
+def lam_pref(word):
+    """
+    check if the lam 'ل' is a prefix of not, and remove it if it was
+    """
+    if len(word) > 3:
+        if check_lexicon(word): return check_lexicon(word)
+        elif word[1] in ['أ', 'ي', 'ت', 'ن']:
+            return word[2:]
+        else: return word
+    else: return word
+
+def baa_pref(word):
+    """
+    check if the baa 'ب' is a prefix of not, and remove it if it was
+    """
+    if len(word) > 3:
+        if check_lexicon(word): return check_lexicon(word)
+        elif word[1:3] == 'ال':
+            return al_pref(word[1:])
+        else: return word
+    else: return word
+
+def kaph_pref(word):
+    """
+    check if the kaph 'ك' is a prefix of not, and remove it if it was
+    """
+    if len(word) > 3:
+        if check_lexicon(word): return check_lexicon(word)
+        elif word[1:3] == 'ال':
+            return al_pref(word[1:])
+        else: return word
+    else: return word
+
+def faa_pref(word):
+    """
+    check if the faa 'ف' is a prefix of not, and remove it if it was
+    """
+    if len(word) > 3:
+        if check_lexicon(word): return check_lexicon(word)
+        elif word[1] == 'ب':
+            return baa_pref(word[1:])
+        elif word[1] == 'ك':
+            return kaph_pref(word[1:])
+        elif word[1:3] == 'ال':
+            return al_pref(word[1:])
+        elif word[1] == 'س':
+            return sen_pref(word[1:])
+        elif word[1] == 'ل':
+            return lam_pref(word[1:])
+        elif word[1] == 'أ':
+            return hamza_pref(word[1:])
+        else: return word
+    else: return word
+
+def waw_pref(word):
+    """
+    check if the waw 'و' is a prefix of not, and remove it if it was
+    """
+    if len(word) > 3:
+        if check_lexicon(word): 
+            return check_lexicon(word)
+        elif word[1] == 'ب':
+            return baa_pref(word[1:])
+        elif word[1] == 'ك':
+            return kaph_pref(word[1:])
+        elif word[1:3] == 'ال':
+            return al_pref(word[1:])
+        elif word[1] == 'س':
+            return sen_pref(word[1:])
+        elif word[1] == 'ل':
+            return lam_pref(word[1:])
+        elif word[1] == 'أ':
+            return hamza_pref(word[1:])
+        else: return word
+    else: return word
+
+duplicate_p_letters = ['تت', 'بب', 'كك', 'فف', 'لل', 'وو']
+multiletter = ['نست', 'لن', 'مست', 'سي', 'سن', 'ست', 'سأ', 'تنن', 'است', 'ات']
+
+def multiletter_pre(word):
+    x, p = '', ''
+    for i in word:
+        x += i
+        if x in multiletter:
+            p = x
+    if p != '': return word[len(p):]
+    else: return False
+
+def pref_handler(word):
+    """
+    Handling the prefix of a word
+    """
+    if len(word) > 3:
+        if word[0:2] in duplicate_p_letters: return word[1:]
+        elif multiletter_pre(word): return multiletter_pre(word)
+        elif word[0] == 'س':
+            return sen_pref(word)
+        elif word[0:2] == 'ال' and len(word) > 4:
+            return al_pref(word)
+        elif word[0] == 'أ':
+            return hamza_pref(word)
+        elif word[0] == 'ل':
+            return lam_pref(word)
+        elif word[0] == 'ب':
+            return baa_pref(word)
+        elif word[0] == 'ك':
+            return kaph_pref(word)
+        elif word[0] == 'ف':
+            return faa_pref(word)
+        elif word[0] == 'و':
+            return waw_pref(word)
+    else: return word
+    return word
